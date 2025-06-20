5f99fe734a42b86c7dfdb30a7289e038ebd86def
JuanCaballero09 <juanes0921200@gmail.com>
2025-06-20 17:31:56 -0500

    Solución de errores

--- Ruby-Proyect/config/importmap.rb
+++ Ruby-Proyect/config/importmap.rb
@@ -2,4 +2,4 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/turbo", to: "turbo.js" # opcional, puede eliminarse si no usas directo esto
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # si lo necesitas
-pin "script", preload: true
+pin "script", to: "script.js"
--- Ruby-Proyect/config/routes.rb
+++ Ruby-Proyect/config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
 
   # Rotas del page
   get "carrito", to: "pages#carrito", as: "carrito"
-  post "agregar_al_carrito", to: "pages#agregar_al_carrito", as: "agregar_al_carrito"
-  post "carrito/eliminar", to: "pages#eliminar_del_carrito", as: "eliminar_del_carrito"
-  get "formulario/:id", to: "pages#formulario", as: "formulario"
 
   get "editar", to: "pages#edit", as: "edit"
   get "menu", to: "pages#menu", as: "menu"
   get "categoria/:nombre", to: "pages#grupo", as: "productos_por_grupo"
   get "producto/:id", to: "pages#productos", as: "producto"
+  get "comprar/:id", to: "pages#formulario", as: "formulario"
+  post "agregar_al_carrito", to: "pages#agregar_al_carrito", as: "agregar_al_carrito"
+  post "carrito/eliminar", to: "pages#eliminar_del_carrito", as: "eliminar_del_carrito"
 
   resources :pedidos, only: [:create] # rubocop:disable Layout/SpaceInsideArrayLiteralBrackets
   namespace :dashboard do
