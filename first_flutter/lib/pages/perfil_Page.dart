539d250d50147318d0de23d030e382943677c87e
andrw790 <andrwbarrera79@gmail.com>
2025-06-25 18:35:17 -0500

    listo

--- first_flutter/lib/pages/perfil_Page.dart
+++ first_flutter/lib/pages/perfil_Page.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 
-class PerfilPage extends StatefulWidget{
+class PerfilPage extends StatelessWidget {
+  const PerfilPage({super.key});
 
   @override
-  PerfilPageState createState() => PerfilPageState();
-}
-
-class PerfilPageState extends State <PerfilPage>{
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      automaticallyImplyLeading: false,
-      backgroundColor: const Color(0xFFFF936B),
-      title: Center(
-        child: Image.asset(
-          'assets/imagen5.png',
-              width: 170,
-        ),
-
-        ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+        centerTitle: true,
       ),
-
-  );
-}
-
+      body: Center(
+        child: ElevatedButton.icon(
+  onPressed: () {
+    // Elimina todo lo anterior y vuelve a la pantalla Welcome
+    Navigator.pushNamedAndRemoveUntil(
+      context,
+      '/welcome',
+      (route) => false,
+    );
+  },
+  icon: Icon(Icons.logout),
+  label: Text('Cerrar sesión'),
+  style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.red[600],
+       foregroundColor: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+       ),
+      ),
+    );
+  }
 }
