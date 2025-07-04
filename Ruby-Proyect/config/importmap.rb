7a3ea65c94c29ab2c3c0849c80babf25f14e1d0c
Juan Caballero <168228657+JuanCaballero09@users.noreply.github.com>
2025-07-04 17:04:10 -0500

    Feature/159 (#161)
    * fin
    
    * update
    
    * update index-dashboard
    
    * agregar contador de productos activos

--- Ruby-Proyect/config/importmap.rb
+++ Ruby-Proyect/config/importmap.rb
@@ -3,3 +3,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/turbo", to: "turbo.js" # opcional, puede eliminarse si no usas directo esto
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # si lo necesitas
 pin "script", to: "script.js"
+pin "chart", to: "chart.js"
