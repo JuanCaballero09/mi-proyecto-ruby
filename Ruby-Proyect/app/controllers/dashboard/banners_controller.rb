30bf66c2cedcc7258b94a56ed52abab6d1dec7e2
san5472 <santiagozambrano751@gmail.com>
2025-07-11 00:11:08 -0500

    Arreglos

--- Ruby-Proyect/app/controllers/dashboard/banners_controller.rb
+++ Ruby-Proyect/app/controllers/dashboard/banners_controller.rb
@@ -1,12 +1,22 @@
 class Dashboard::BannersController < ApplicationController
-  def index
+   def index
+    @banner = Banner.new
+    @banners = Banner.all
   end
 
   def create
-    @banner = Banner.new
-    @banner.save 
+    @banner = Banner.new(banner_params)
+    @banner.save
   end
 
   def destroy
+    banner = Banner.find(params[:id])
+    banner.destroy
+  end
+
+  private
+
+  def banner_params
+    params.require(:banner).permit(:image)
   end
 end
