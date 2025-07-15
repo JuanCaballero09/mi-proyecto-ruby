f909429596355805b5c9e1a011c9130490faad6e
JuanCaballero09 <juanes0921200@gmail.com>
2025-07-15 13:43:01 -0500

    fin

--- Ruby-Proyect/db/seeds.rb
+++ Ruby-Proyect/db/seeds.rb
@@ -2,16 +2,18 @@ Grupo.destroy_all
 User.destroy_all
 Product.destroy_all
 Ingrediente.destroy_all
+IngredienteProducto.destroy_all
 
 ActiveRecord::Base.connection.reset_pk_sequence!('grupos')
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!('products')
+ActiveRecord::Base.connection.reset_pk_sequence!('ingredientes')
 
 puts "Creando Grupos..."
-Grupo.create(nombre: "Grupo 1")
-Grupo.create(nombre: "Grupo 2")
-Grupo.create(nombre: "Grupo 3")
-puts "Grupos creados #{Grupo.count}"
-
-ActiveRecord::Base.connection.reset_pk_sequence!('users')
+grupo1 = Grupo.create(nombre: "Hamburguesas", descripcion: "Jugosas hamburguesas artesanales")
+grupo2 = Grupo.create(nombre: "Salchipapas", descripcion: "Papas con variedad de toppings")
+grupo3 = Grupo.create(nombre: "Pizzas", descripcion: "Pizzas al horno con masa artesanal")
+puts "Grupos creados: #{Grupo.count}"
 
 puts "Creando Usuario Admin..."
 User.create(
@@ -21,82 +23,89 @@ User.create(
   apellido: "Admin",
   telefono: "123456789",
   rol: :admin,
-  confirmed_at: Time.current # Encargado de que si el correo esta verificado lo puede dejar entrar
+  confirmed_at: Time.current
 )
-
-puts "Usuarios creados #{User.count}"
-
-
-ActiveRecord::Base.connection.reset_pk_sequence!('products')
-
-puts "Creando productos..."
-
-grupo1 = Grupo.find_by(nombre: "Grupo 1")
-grupo2 = Grupo.find_by(nombre: "Grupo 2")
-grupo3 = Grupo.find_by(nombre: "Grupo 3")
-
-# Productos Grupo 1
-Product.create(nombre: "Producto 1 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 1", disponible: true, grupo_id: grupo1.id)
-Product.create(nombre: "Producto 2 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 2", disponible: true, grupo_id: grupo1.id)
-Product.create(nombre: "Producto 3 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 3", disponible: true, grupo_id: grupo1.id)
-Product.create(nombre: "Producto 4 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 4", disponible: true, grupo_id: grupo1.id)
-Product.create(nombre: "Producto 5 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 5", disponible: true, grupo_id: grupo1.id)
-Product.create(nombre: "Producto 6 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 6", disponible: true, grupo_id: grupo1.id)
-Product.create(nombre: "Producto 7 (Grupo 1)", precio: 99999, descripcion: "Descripción producto 7", disponible: true, grupo_id: grupo1.id)
-
-# Productos Grupo 2
-Product.create(nombre: "Producto 1 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 1", disponible: true, grupo_id: grupo2.id)
-Product.create(nombre: "Producto 2 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 2", disponible: true, grupo_id: grupo2.id)
-Product.create(nombre: "Producto 3 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 3", disponible: true, grupo_id: grupo2.id)
-Product.create(nombre: "Producto 4 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 4", disponible: true, grupo_id: grupo2.id)
-Product.create(nombre: "Producto 5 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 5", disponible: true, grupo_id: grupo2.id)
-Product.create(nombre: "Producto 6 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 6", disponible: true, grupo_id: grupo2.id)
-Product.create(nombre: "Producto 7 (Grupo 2)", precio: 99999, descripcion: "Descripción producto 7", disponible: true, grupo_id: grupo2.id)
-
-# Productos Grupo 3
-Product.create(nombre: "Producto 1 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 1", disponible: true, grupo_id: grupo3.id)
-Product.create(nombre: "Producto 2 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 2", disponible: true, grupo_id: grupo3.id)
-Product.create(nombre: "Producto 3 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 3", disponible: true, grupo_id: grupo3.id)
-Product.create(nombre: "Producto 4 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 4", disponible: true, grupo_id: grupo3.id)
-Product.create(nombre: "Producto 5 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 5", disponible: true, grupo_id: grupo3.id)
-Product.create(nombre: "Producto 6 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 6", disponible: true, grupo_id: grupo3.id)
-Product.create(nombre: "Producto 7 (Grupo 3)", precio: 99999, descripcion: "Descripción producto 7", disponible: true, grupo_id: grupo3.id)
-
-puts "Productos creados: #{Product.count}"
-
+puts "Usuarios creados: #{User.count}"
 
 puts "Creando Ingredientes..."
 
-ingredientes = [
-  "Carne de res",
-  "Pollo desmechado",
-  "Salchicha",
-  "Chorizo",
-  "Tocineta",
-  "Queso mozzarella",
-  "Queso cheddar",
-  "Huevo",
-  "Jamón",
-  "Lechuga",
-  "Tomate",
-  "Cebolla",
-  "Cebolla caramelizada",
-  "Maíz tierno",
-  "Pepinillos",
-  "Papas a la francesa",
-  "Papas ripio",
-  "Salsas (rosada, tártara, BBQ)",
-  "Ajo triturado",
-  "Guacamole",
-  "Pan de hamburguesa",
-  "Pan de perro caliente",
-  "Arepa",
-  "Tortilla de harina",
-  "Tortilla de maíz"
+nombres_ingredientes = [
+  "Carne de res", "Pollo desmechado", "Salchicha", "Chorizo", "Tocineta", "Queso mozzarella",
+  "Queso cheddar", "Huevo", "Jamón", "Lechuga", "Tomate", "Cebolla", "Cebolla caramelizada",
+  "Maíz tierno", "Pepinillos", "Papas a la francesa", "Papas ripio", "Salsas (rosada, tártara, BBQ)",
+  "Ajo triturado", "Guacamole", "Pan de hamburguesa", "Pan de perro caliente",
+  "Arepa", "Tortilla de harina", "Tortilla de maíz", "Piña", "Salsa de tomate", "Orégano"
 ]
 
-ingredientes.each do |nombre|
-  Ingrediente.find_or_create_by(nombre: nombre)
+ingredientes = {}
+nombres_ingredientes.each do |nombre|
+  ingredientes[nombre] = Ingrediente.find_or_create_by(nombre: nombre)
+end
+puts "Ingredientes creados: #{Ingrediente.count}"
+
+puts "Creando productos..."
+
+# Hamburguesas
+hamburguesas = [
+  { nombre: "Hamburguesa Clásica", ingredientes: [ "Carne de res", "Queso cheddar", "Lechuga", "Tomate", "Pan de hamburguesa", "Salsas (rosada, tártara, BBQ)" ] },
+  { nombre: "Hamburguesa BBQ", ingredientes: [ "Carne de res", "Tocineta", "Queso mozzarella", "Cebolla caramelizada", "Pan de hamburguesa" ] },
+  { nombre: "Hamburguesa de Pollo", ingredientes: [ "Pollo desmechado", "Lechuga", "Tomate", "Pan de hamburguesa", "Guacamole" ] },
+  { nombre: "Hamburguesa Mexicana", ingredientes: [ "Carne de res", "Ajo triturado", "Guacamole", "Pan de hamburguesa" ] },
+  { nombre: "Hamburguesa Doble Carne", ingredientes: [ "Carne de res", "Carne de res", "Queso cheddar", "Pan de hamburguesa", "Salsas (rosada, tártara, BBQ)" ] },
+  { nombre: "Hamburguesa Hawaiana", ingredientes: [ "Carne de res", "Jamón", "Piña", "Queso mozzarella", "Pan de hamburguesa" ] },
+  { nombre: "Hamburguesa Criolla", ingredientes: [ "Carne de res", "Huevo", "Queso mozzarella", "Cebolla", "Pan de hamburguesa" ] }
+]
+
+hamburguesas.each do |data|
+  producto = Product.create(
+    nombre: data[:nombre],
+    precio: 15000,
+    descripcion: "Deliciosa #{data[:nombre]}",
+    disponible: true,
+    grupo_id: grupo1.id
+  )
+  data[:ingredientes].each do |ing|
+    producto.ingredientes << ingredientes[ing]
+  end
 end
 
-puts "Ingredientes creados: #{Ingrediente.count}"
+# Salchipapas
+salchipapas = [
+  { nombre: "Salchipapa Clásica", ingredientes: [ "Papas a la francesa", "Salchicha", "Salsas (rosada, tártara, BBQ)" ] },
+  { nombre: "Salchipapa Mixta", ingredientes: [ "Papas a la francesa", "Chorizo", "Salchicha", "Queso cheddar", "Salsas (rosada, tártara, BBQ)" ] }
+]
+
+salchipapas.each do |data|
+  producto = Product.create(
+    nombre: data[:nombre],
+    precio: 12000,
+    descripcion: "Deliciosa #{data[:nombre]}",
+    disponible: true,
+    grupo_id: grupo2.id
+  )
+  data[:ingredientes].each do |ing|
+    producto.ingredientes << ingredientes[ing]
+  end
+end
+
+# Pizzas
+pizzas = [
+  { nombre: "Pizza Hawaiana", ingredientes: [ "Queso mozzarella", "Jamón", "Piña", "Salsa de tomate", "Orégano" ] },
+  { nombre: "Pizza de Carne", ingredientes: [ "Carne de res", "Queso mozzarella", "Cebolla", "Salsa de tomate" ] },
+  { nombre: "Pizza Vegetariana", ingredientes: [ "Queso mozzarella", "Tomate", "Cebolla", "Maíz tierno", "Orégano" ] }
+]
+
+pizzas.each do |data|
+  producto = Product.create(
+    nombre: data[:nombre],
+    precio: 18000,
+    descripcion: "Deliciosa #{data[:nombre]}",
+    disponible: true,
+    grupo_id: grupo3.id
+  )
+  data[:ingredientes].each do |ing|
+    producto.ingredientes << ingredientes[ing]
+  end
+end
+
+puts "Productos creados: #{Product.count}"
