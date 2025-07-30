6ca9247b19a7d94c76ba84764467d4c0f2b14d6d
san5472 <santiagozambrano751@gmail.com>
2025-07-30 00:06:52 -0500

    Adelanto

--- Ruby-Proyect/app/controllers/dashboard/dashboard_controller.rb
+++ Ruby-Proyect/app/controllers/dashboard/dashboard_controller.rb
@@ -6,6 +6,7 @@ class Dashboard::DashboardController < ApplicationController
 
 
   def index
+    load_charts_data
   end
 
   private
@@ -15,4 +16,10 @@ class Dashboard::DashboardController < ApplicationController
       redirect_to root_path, alert: "No tienes acceso a esta página." # Redirige si no es admin
     end
   end
+
+  def load_charts_data
+    @products_by_group = Product.joins(:grupo).group('grupos.nombre').count
+    @grupos_growth = Grupo.group_by_month(:created_at, format: "%b %Y").count
+  end
+
 end
